1bc00c8abf06df891baa7ade1ec5347e8057373a
John <juan.silvas@live.com>
2019-10-05 20:33:03 -0400

    adding files

---
+++ App.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+const playstore = require('./playstore.js')
+
+app.get('/apps', (req, res) => {
+
+  const { search = ""} = req.query;
+
+  let results = playstore.filter(playstore => playstore.Genres.toLowerCase().includes(search.toLowerCase()));
+  console.log(playstore.Genres.toLowerCase().includes(search.toLowerCase()));
+  res.json(results);
+
+});
+
+app.listen(8000, () => {
+  console.log('Express server is listening on port 8000!');
+
+});
